d2f192aec2ccda529e7725d42c42249f03fa2449
Sagar Sathe <sssathesagar@gmail.com>
2023-03-29 00:42:58 +0530

    Initial commit

---
+++ src/main/java/com/sathebiz/truecaller/vo/BaseResponse.java
@@ -0,0 +1,42 @@
+package com.sathebiz.truecaller.vo;
+
+public class BaseResponse {
+
+	private String messageCode;
+	private String message;
+	private Integer status;
+	private Object data;
+
+	public String getMessageCode() {
+		return messageCode;
+	}
+
+	public void setMessageCode(String messageCode) {
+		this.messageCode = messageCode;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+}
